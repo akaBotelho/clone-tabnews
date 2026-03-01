f17f636f95d460e366c45b90e074c64b86ac5e67
Guilherme Botelho <guilhermehcb@hotmail.com>
2026-03-01 20:51:01 -0300

    feat: return database info in status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,13 +1,24 @@
 import database from "infra/database.js";
 
 async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1 AS sum;");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
+  const databaseVersion = await database.query("SHOW server_version");
+  const databaseMaxConnections = await database.query("SHOW max_connections");
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
+    values: [databaseName],
+  });
+
   res.status(200).json({
-    status: "ok",
-    version: "1.0.0",
-    uptime: process.uptime(),
-    timestamp: new Date().toISOString(),
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersion.rows[0].server_version,
+        max_connections: parseInt(databaseMaxConnections.rows[0].max_connections),
+        open_connections: databaseOpenedConnections.rows[0].count,
+      },
+    },
   });
 }
 
